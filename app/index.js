f7d79971ce2b5c725d8a78a760fe1bbdb626f2dd
Nuno Balbona <niciusb@gmail.com>
2017-09-22 19:52:09 +0200

    Added phaser

--- app/index.js
+++ app/index.js
@@ -6,7 +6,21 @@
 import 'styles/index.scss';
 
 // ================================
-// START YOUR APP HERE
+// APP HERE
 // ================================
 
-console.log('huh')
+window.PIXI = require('phaser-ce/build/custom/pixi')
+window.p2 = require('phaser-ce/build/custom/p2')
+window.Phaser = require('phaser-ce/build/custom/phaser-split')
+
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-game', { preload: preload, create: create })
+
+function preload() {
+    game.load.image('einstein', 'assets/images/einstein.png')
+
+}
+
+function create() {
+    var s = game.add.sprite(80, 0, 'einstein')
+    s.rotation = 0.14
+}
