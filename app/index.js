27e2d117e902b228b7df83ac7e4bfde9b5597290
Nuno Balbona <niciusb@gmail.com>
2017-09-23 01:33:30 +0200

    Initial objects

--- app/index.js
+++ app/index.js
@@ -3,24 +3,16 @@
  */
 
 // Load application styles
-import 'styles/index.scss';
+import 'styles/index.scss'
 
-// ================================
-// APP HERE
-// ================================
-
+// Game
 window.PIXI = require('phaser-ce/build/custom/pixi')
 window.p2 = require('phaser-ce/build/custom/p2')
 window.Phaser = require('phaser-ce/build/custom/phaser-split')
 
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-game', { preload: preload, create: create })
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-game', null, false, false)
 
-function preload() {
-    game.load.image('einstein', 'assets/images/einstein.png')
+const GameState = require('classes/GameState.js')
+game.state.add('GameState', GameState)
 
-}
-
-function create() {
-    var s = game.add.sprite(80, 0, 'einstein')
-    s.rotation = 0.14
-}
+game.state.start('GameState')
