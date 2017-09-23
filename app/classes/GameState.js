7686805b8831d5dc84e7589ff153a5c5349eabe6
Nuno Balbona <niciusb@gmail.com>
2017-09-23 12:36:54 +0200

    Updated hitboxes and added p2 debug

--- app/classes/GameState.js
+++ app/classes/GameState.js
@@ -10,15 +10,14 @@ class GameState extends Phaser.State {
   create() {
     this.game.world.setBounds(0, 0, 3840, 600)
     this.game.physics.startSystem(Phaser.Physics.P2JS)
-    this.game.physics.p2.restitution = 0
     this.game.physics.p2.world.defaultContactMaterial.friction = 0.5
+    this.game.physics.p2.world.setGlobalStiffness(1e5)
 
     this.game.add.sprite(0, 0, 'stage01')
 
     var mesa = this.game.add.sprite(300, this.game.world.centerY, 'mesa_1')
-    this.game.physics.p2.enable(mesa)
+    this.game.physics.p2.enable(mesa, true)
     mesa.body.kinematic = true
-    mesa.anchor.set(.5)
 
     const Player = require('./Player.js')
     var player = this.add.existing(new Player(this.game, 100, 100))
--- app/classes/Player.js
+++ app/classes/Player.js
@@ -1,11 +1,12 @@
 class Player extends Phaser.Sprite {
   constructor(game, x, y) {
     super(game, x, y, 'playerSprite', 1)
-    game.physics.p2.enable(this)
+    game.physics.p2.enable(this, true)
     this.body.fixedRotation = true
+    this.body.clearShapes()
+    this.body.addRectangle(65, 80, -10, 0)
 
-    this.anchor.set(.5)
-    this.speed = 20
+    this.speed = 200
 
     this.animations.add('hi', [0,1], 5, true)
     this.play('hi')
@@ -16,18 +17,20 @@ class Player extends Phaser.Sprite {
     this.body.velocity.x *= 12.5 / delta // We must use something smaller than 16
     this.body.velocity.y *= 12.5 / delta
     if (this.game.input.keyboard.isDown(Phaser.KeyCode.S)) {
-      this.body.moveDown(delta * this.speed)
+      this.body.moveDown(this.speed)
     }
     if (this.game.input.keyboard.isDown(Phaser.KeyCode.W)) {
-      this.body.moveUp(delta * this.speed)
+      this.body.moveUp(this.speed)
     }
     if (this.game.input.keyboard.isDown(Phaser.KeyCode.A)) {
-      this.body.moveLeft(delta * this.speed)
+      this.body.moveLeft(this.speed)
       this.scale.x = -1
+      this.body.angle = 180
     }
     if (this.game.input.keyboard.isDown(Phaser.KeyCode.D)) {
-      this.body.moveRight(delta * this.speed)
+      this.body.moveRight(this.speed)
       this.scale.x = 1
+      this.body.angle = 0
     }
   }
 }
