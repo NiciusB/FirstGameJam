fb6387c1eb5cc183435e53091389a033da8bde1c
Nuno Balbona <niciusb@gmail.com>
2017-09-23 23:11:28 +0200

    Improved Enemy modularity

--- app/classes/Enemy.js
+++ app/classes/Enemy.js
@@ -4,10 +4,64 @@ class Enemy extends Alive {
     super(game, x, y, sprite)
     game.physics.p2.enable(this)
     this.body.fixedRotation = true
+
+    // Attributes
+    this.speed = 0
+    this.meleeAttackRange = false
+    this.meleeAttackPower = false
+    // Internal vars
+    this.msInAttackRange = 0
   }
 
   update() {
     super.update()
+    const delta = this.game.time.elapsedMS // Delta for 60fps is 16.66
+
+    if (this.inCamera) {
+      // meleeAttack
+      if (this.meleeAttackRange && this.meleeAttackPower && Phaser.Math.distance(this.game.player.x, this.game.player.y / 1.5, this.x, this.y / 1.5) <= this.meleeAttackRange) {
+        this.play('meleeAttack')
+        this.msInAttackRange += delta
+        while (this.msInAttackRange > 500) {
+          this.msInAttackRange -= 500
+          this.game.player.damage(this.meleeAttackPower)
+        }
+        if (this.x > this.game.player.x) {
+          this.body.moveLeft(this.speed / 25)
+        } else {
+          this.body.moveRight(this.speed / 25)
+        }
+        if (this.y > this.game.player.y) {
+          this.body.moveUp(this.speed / 25)
+        } else {
+          this.body.moveDown(this.speed / 25)
+        }
+      }
+      // Pathfinding
+      if (this.animations.currentAnim.name != 'meleeAttack') {
+        this.msInAttackRange = 0
+        const closeEnough = 5 // in pixels
+        if (Math.round(this.x / closeEnough) != Math.round(this.game.player.x / closeEnough)) {
+          if (this.x > this.game.player.x) {
+            this.body.moveLeft(this.speed)
+            this.play('walking')
+          } else {
+            this.body.moveRight(this.speed)
+            this.play('walking')
+          }
+        }
+        if (Math.round(this.y / closeEnough) != Math.round(this.game.player.y / closeEnough)) {
+          if (this.y > this.game.player.y) {
+            this.body.moveUp(this.speed)
+            this.play('walking')
+          } else {
+            this.body.moveDown(this.speed)
+            this.play('walking')
+          }
+        }
+      }
+
+    }
   }
 }
 
--- app/classes/Skeleton.js
+++ app/classes/Skeleton.js
@@ -8,64 +8,20 @@ class Skeleton extends Enemy {
     // Attributes
     this.speed = 100
     this.meleeAttackRange = 70
-    this.attackPower = 15
-
-    // Internal vars
-    this.msInAttackRange = 0
+    this.meleeAttackPower = 15
 
     this.animations.add('standing', [0], 0, false)
     this.animations.add('walking', [1, 2], 4, false)
-    this.animations.add('attacking', [3, 4], 4, false)
+    this.animations.add('meleeAttack', [3, 4], 4, false)
     this.play('standing')
+    
+    this.events.onAnimationComplete.add(() => {
+      this.play('standing')
+    }, this)
   }
 
   update() {
     super.update()
-    const delta = this.game.time.elapsedMS // Delta for 60fps is 16.66
-
-    if (this.inCamera) {
-      // ataque de esqueleto
-      if (Phaser.Math.distance(this.game.player.x, this.game.player.y / 1.5, this.x, this.y / 1.5) <= this.meleeAttackRange) {
-        this.play('attacking')
-        this.msInAttackRange += delta
-        while (this.msInAttackRange > 500) {
-          this.msInAttackRange -= 500
-          this.game.player.damage(this.attackPower)
-        }
-        if (this.x > this.game.player.x) {
-          this.body.moveLeft(this.speed / 25)
-        } else {
-          this.body.moveRight(this.speed / 25)
-        }
-        if (this.y > this.game.player.y) {
-          this.body.moveUp(this.speed / 25)
-        } else {
-          this.body.moveDown(this.speed / 25)
-        }
-      } else {
-        this.msInAttackRange = 0
-        // pathfinding
-        const closeEnough = 5 // in pixels
-        if (Math.round(this.x / closeEnough) != Math.round(this.game.player.x / closeEnough)) {
-          if (this.x > this.game.player.x) {
-            this.body.moveLeft(this.speed)
-            this.play('walking')
-          } else {
-            this.body.moveRight(this.speed)
-            this.play('walking')
-          }
-        }
-        if (Math.round(this.y / closeEnough) != Math.round(this.game.player.y / closeEnough)) {
-          if (this.y > this.game.player.y) {
-            this.body.moveUp(this.speed)
-            this.play('walking')
-          } else {
-            this.body.moveDown(this.speed)
-            this.play('walking')
-          }
-        }
-      }
-    }
   }
 }
 
