aca90d809f8956ae6b6a5ef909d17f4ec2a2661b
Nuno Balbona <niciusb@gmail.com>
2017-09-23 11:59:54 +0200

    Added physics

--- app/classes/GameState.js
+++ app/classes/GameState.js
@@ -4,11 +4,21 @@ class GameState extends Phaser.State {
   }
   preload() {
     this.load.image('stage01', 'assets/images/stage/01.png')
-    this.load.spritesheet('playerSprite', 'assets/images/playerSprite.png', 17, 28)
+    this.load.image('table', 'assets/images/stage/table.png')
+    this.load.spritesheet('playerSprite', 'assets/images/playerSprite.png', 85, 140)
   }
   create() {
+    this.game.world.setBounds(0, 0, 3840, 600)
+    this.game.physics.startSystem(Phaser.Physics.P2JS)
+    this.game.physics.p2.restitution = 0
+    this.game.physics.p2.world.defaultContactMaterial.friction = 0.5
+
     this.game.add.sprite(0, 0, 'stage01')
-    this.game.world.setBounds(0, 0, 1920, 1920)
+
+    var mesa = this.game.add.sprite(300, this.game.world.centerY, 'table')
+    this.game.physics.p2.enable(mesa)
+    mesa.body.kinematic = true
+    mesa.anchor.set(.5)
 
     const Player = require('./Player.js')
     var player = this.add.existing(new Player(this.game, 100, 100))
--- app/classes/Player.js
+++ app/classes/Player.js
@@ -1,27 +1,33 @@
 class Player extends Phaser.Sprite {
   constructor(game, x, y) {
     super(game, x, y, 'playerSprite', 1)
+    game.physics.p2.enable(this)
+    this.body.fixedRotation = true
+
     this.anchor.set(.5)
-    this.scale.set(5)
-    this.customCounter = 0
     this.speed = 20
+
     this.animations.add('hi', [0,1], 5, true)
     this.play('hi')
   }
 
   update() {
-    const delta = this.game.time.elapsedMS
+    const delta = this.game.time.elapsedMS // Delta for 60fps is 16.66
+    this.body.velocity.x *= 12.5 / delta // We must use something smaller than 16
+    this.body.velocity.y *= 12.5 / delta
     if (this.game.input.keyboard.isDown(Phaser.KeyCode.S)) {
-      this.y += delta * this.speed / 100
+      this.body.moveDown(delta * this.speed)
     }
     if (this.game.input.keyboard.isDown(Phaser.KeyCode.W)) {
-      this.y -= delta * this.speed / 100
+      this.body.moveUp(delta * this.speed)
     }
     if (this.game.input.keyboard.isDown(Phaser.KeyCode.A)) {
-      this.x -= delta * this.speed / 100
+      this.body.moveLeft(delta * this.speed)
+      this.scale.x = -1
     }
     if (this.game.input.keyboard.isDown(Phaser.KeyCode.D)) {
-      this.x += delta * this.speed / 100
+      this.body.moveRight(delta * this.speed)
+      this.scale.x = 1
     }
   }
 }
